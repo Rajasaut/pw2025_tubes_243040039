811a0cbd4bb1801805c05846219ab84e1bc8bb68
Rajasaut <rajasaut99@gmail.com>
2025-05-20 00:50:55 +0700

    mengedit sedikit bagian koneksi

--- Raja-243040039/php/koneksi.php
+++ Raja-243040039/php/koneksi.php
@@ -1,12 +1,19 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "nama_database";
-$dbname = "bengkel_db";
+//coneksi ke db & pilih Databese
+$conn = mysqli_connect('localhost', 'root', '', 'bengkel_db');
+
+//query isi tabel mahasiswa
+$result = mysqli_query($conn, "SELECT * FROM users");
+
+//<-------------------------------------- cara ke 2 untuk mengkonekan ke db & pilih database-------------------------------------------->//
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "nama_database";
+// $dbname = "bengkel_db";
 
 // Membuat koneksi ke database
-$conn = new mysqli($servername, $username, $password, $dbname);
+// $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Untuk Cek koneksi
 if ($conn->connect_error) {
